f35f30175b03d9d99095e44f1b2befba26c2fd9d
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-06-29 15:01:48 -0400

    randomized nicknames

--- models/name_generator.rb
+++ models/name_generator.rb
@@ -10,63 +10,12 @@ class NameGenerator
   end
 
   def nickname
-    version = "#{rand(0..6)}.#{rand(0..6)}.#{rand(0..6)}"
-    if name.start_with?('a','A') 
-      nickname = "Attr-Accessorizing #{name.capitalize} #{version}"
-    elsif name.start_with?('b', 'B')
-      nickname = "Debugging #{name.capitalize} #{version}" 
-    elsif name.start_with?('c', 'C')
-      nickname = "Coding #{name.capitalize} #{version}"
-    elsif name.start_with?('d', 'D')
-      nickname = "DB:Migrating #{name.capitalize} #{version}"
-    elsif name.start_with?('e', 'E')
-      nickname = "Email-Parsing #{name.capitalize} #{version}"
-    elsif name.start_with?('f', 'F')
-      nickname = "Full-Snack #{name.capitalize} #{version}"
-    elsif name.start_with?('g', 'G')
-      nickname = "Gemified #{name.capitalize} #{version}"
-    elsif name.start_with?('h', 'H')
-      nickname = "Hashketballing #{name.capitalize} #{version}"
-    elsif name.start_with?('i', 'I')
-      nickname = "Iterating #{name.capitalize} #{version}"
-    elsif name.start_with?('j', 'J')
-      nickname = "Joining-Tables #{name.capitalize} #{version}"
-    elsif name.start_with?('k', 'K')
-      nickname = "Keywording #{name.capitalize} #{version}" 
-    elsif name.start_with?('l', 'L')
-      nickname = "Loopy #{name.capitalize} #{version}" 
-    elsif name.start_with?('m', 'M')
-      nickname = "Method #{name.capitalize} #{version}"
-    elsif name.start_with?('n', 'N')
-      nickname = "Nokogiri #{name.capitalize} #{version}"
-    elsif name.start_with?('o', 'O')
-      nickname = "Object-Oriented #{name.capitalize} #{version}"
-    elsif name.start_with?('p', 'P')
-      nickname = "Playlisting #{name.capitalize} #{version}"
-    elsif name.start_with?('q', 'Q')
-      nickname = "Quick #{name.capitalize} #{version}" 
-    elsif name.start_with?('r', 'R')
-      nickname = "#{name.capitalize} on Rails #{version}"
-    elsif name.start_with?('s', 'S')
-      nickname = "Syntactic-Sugaring #{name.capitalize} #{version}"
-    elsif name.start_with?('t', 'T')
-      nickname = "Travis-Hating #{name.capitalize} #{version}"
-    elsif name.start_with?('u', 'U')
-      nickname = "Uniq #{name.capitalize} #{version}"
-    elsif name.start_with?('v', 'V')
-      nickname = "Varchar #{name.capitalize} #{version}"
-          elsif name.start_with?('w', 'W')
-      nickname = "Winning #{name.capitalize} #{version}"
-    elsif name.start_with?('x', 'X')
-      nickname = "XXX #{name.capitalize}"
-    elsif name.start_with?('y', 'Z')
-      nickname = "Yielding #{name.capitalize} #{version}"
-          elsif name.start_with?('z', 'Z')
-      nickname = "Zealous #{name.capitalize} #{version}"
-    else
-      nickname = "That wasn't valid - try again!"
-    end
-    nickname
+    version = "#{rand(0..9)}.#{rand(0..9)}.#{rand(0..9)}"
+    adjectives = ["Magical", "Mesmerizing", "Sexy", "Saucy", "Feisty", "Lovable", "Luscious"]
+    codewords = ["Attr-Accessorizing", "Debugging", "Coding", "DB:Migrating", "Email-Parsing", 
+      "Full-Snacking", "Gemifying", "Hashketballing", "Iterating", "Looping", "Object-Oriented", 
+      "Playlisting"]
+    nickname = "#{adjectives.sample} #{codewords.sample} #{name.split.map(&:capitalize).join(' ')} #{version}"
   end
 
 end
